886fce02937d2f14c873e8f6df4efb8541e3ad0f
Seungyeon Lee <sie9412@gmail.com>
2022-07-05 15:26:25 +0900

    build: implement emotion(css-in-js)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
+import { Global, ThemeProvider } from '@emotion/react';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import reset from '../styles/reset';
+import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Global styles={reset} />
+        <Component {...pageProps} />;
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp;
