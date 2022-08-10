af625936f75e6b58e431ce965f92978097556794
Seungyeon Lee <sie9412@gmail.com>
2022-08-10 15:30:37 +0900

    feat: project pages (by its state)

--- src/pages/recruited.tsx
+++ src/pages/recruited.tsx
@@ -1,13 +1,27 @@
 import SecondaryProjectLayout from '@/components/Projects/SecondaryProjectLayout';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import PrimaryLayout from 'src/components/Layouts/PrimaryLayout';
-import { data } from '.';
+import { useAppSelector } from 'src/redux/hooks';
+import { recruitedProject } from 'src/redux/reducers/projects/recruitedProjects';
 
 const Recruited = () => {
+  const token = useAppSelector((state) => state.auth.token);
+
+  const recruitedProjects = useAppSelector(
+    (state) => state.recruitedProjects.projectList
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(recruitedProject({ pageNumber: 0 }));
+  }, [token, dispatch]);
+
   return (
     <PrimaryLayout>
       <SecondaryProjectLayout
         title="모집 완료된 프로젝트"
-        projectDtoList={data}
+        projectDtoList={recruitedProjects}
       />
     </PrimaryLayout>
   );
--- src/pages/recruiting.tsx
+++ src/pages/recruiting.tsx
@@ -1,13 +1,27 @@
 import SecondaryProjectLayout from '@/components/Projects/SecondaryProjectLayout';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import PrimaryLayout from 'src/components/Layouts/PrimaryLayout';
-import { data } from '.';
+import { useAppSelector } from 'src/redux/hooks';
+import { recruitingProject } from 'src/redux/reducers/projects/recruitingProjects';
 
 const Recruiting = () => {
+  const token = useAppSelector((state) => state.auth.token);
+
+  const recruitingProjects = useAppSelector(
+    (state) => state.recruitingProjects.projectList
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(recruitingProject({ pageNumber: 0 }));
+  }, [token, dispatch]);
+
   return (
     <PrimaryLayout>
       <SecondaryProjectLayout
         title="모집 중인 프로젝트"
-        projectDtoList={data}
+        projectDtoList={recruitingProjects}
       />
     </PrimaryLayout>
   );
