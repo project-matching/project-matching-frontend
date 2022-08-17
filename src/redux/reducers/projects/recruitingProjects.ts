6fb8eb524d29103add253031c994abf97a98cf59
Seungyeon Lee <sie9412@gmail.com>
2022-08-17 14:24:40 +0900

    fix: build error

--- src/redux/reducers/projects/recruitingProjects.ts
+++ src/redux/reducers/projects/recruitingProjects.ts
@@ -1,4 +1,6 @@
 import { createAction, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { ProjectRequestType } from './../../../services/ProjectService';
+
 import { ProjectState } from './project';
 
 const initialState: ProjectState = {
@@ -48,7 +50,7 @@ export const recruitingProjectPreview = createAction(
   `${projectState}/recruitingProjectPreview`
 );
 
-export const recruitingProject = createAction<ProjectPreviewType>(
+export const recruitingProject = createAction<ProjectRequestType>(
   `${projectState}/recruitingProject`
 );
 
