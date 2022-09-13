684198fc4c3aef27321e111b5b4da6ac0cab1a39
Seungyeon Lee <sie9412@gmail.com>
2022-09-13 15:39:58 +0900

    fix: 프로필 변경 - 프로필 이미지 업로드 이슈

--- src/components/Dashboard/TechStack/TechstackList.tsx
+++ src/components/Dashboard/TechStack/TechstackList.tsx
@@ -7,39 +7,6 @@ const Title = styled.div`
   margin-bottom: 10px;
 `;
 
-const data = [
-  {
-    image:
-      'https://project-matching-s3.s3.ap-northeast-2.amazonaws.com/741e7a82-2018-4ed0-b1bf-c93cf253e4e2.jpg',
-    technicalStackName: 'Spring',
-    technicalStackNo: 0,
-  },
-  {
-    image:
-      'https://project-matching-s3.s3.ap-northeast-2.amazonaws.com/741e7a82-2018-4ed0-b1bf-c93cf253e4e2.jpg',
-    technicalStackName: 'NextJS',
-    technicalStackNo: 1,
-  },
-  {
-    image:
-      'https://project-matching-s3.s3.ap-northeast-2.amazonaws.com/741e7a82-2018-4ed0-b1bf-c93cf253e4e2.jpg',
-    technicalStackName: 'React',
-    technicalStackNo: 2,
-  },
-  {
-    image:
-      'https://project-matching-s3.s3.ap-northeast-2.amazonaws.com/741e7a82-2018-4ed0-b1bf-c93cf253e4e2.jpg',
-    technicalStackName: 'TypeScript',
-    technicalStackNo: 3,
-  },
-  {
-    image:
-      'https://project-matching-s3.s3.ap-northeast-2.amazonaws.com/741e7a82-2018-4ed0-b1bf-c93cf253e4e2.jpg',
-    technicalStackName: 'NestJS',
-    technicalStackNo: 4,
-  },
-];
-
 const TechstackList = () => {
   const techstacks = useAppSelector((state) => state.techStack.techstacks);
 
