e501a1f5b1cc249d31261cd4a9caa4fd17f8d57e
Seungyeon Lee <sie9412@gmail.com>
2022-09-21 16:14:15 +0900

    fix: api url with dev mode

--- src/services/AppApi.ts
+++ src/services/AppApi.ts
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 export const appApi = axios.create({
   baseURL:
-    'http://ec2-3-39-79-163.ap-northeast-2.compute.amazonaws.com:8080/v1',
+    process.env.NODE_ENV == 'development'
+      ? 'http://localhost:8080/v1'
+      : 'http://ec2-3-39-79-163.ap-northeast-2.compute.amazonaws.com:8080/v1',
   timeout: 2500,
 });
